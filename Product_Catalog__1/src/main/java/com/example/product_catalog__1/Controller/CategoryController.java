b042ec60896f6672355f1abe10859850c98db38b
ittzJonny <mananbansal162@gmail.com>
2025-04-01 22:47:17 +0530

    Cat commit

--- Product_Catalog__1/src/main/java/com/example/product_catalog__1/Controller/CategoryController.java
+++ Product_Catalog__1/src/main/java/com/example/product_catalog__1/Controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.example.product_catalog__1.Services.FakeStoreCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +21,11 @@ public class CategoryController {
     @GetMapping("/")
     public List<String> getAllCategories() {
         return categoriesService.getAllCategories();
+    }
+
+    @RequestMapping(path = "cat", method = RequestMethod.GET)
+    public String getCategory() {
+        return "hey this is cat";
     }
 
 }
