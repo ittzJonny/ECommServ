80c4ddc71cd0f15e3c45b7ac6147da4e544812df
ittzJonny <mananbansal162@gmail.com>
2025-05-01 20:43:30 +0530

    Initialize Heimdall Gateway project with Maven setup and basic application structure

---
+++ Heimdall_GateWay/src/main/java/com/example/heimdall_gateway/HeimdallGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.heimdall_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdallGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeimdallGateWayApplication.class, args);
+    }
+
+}
